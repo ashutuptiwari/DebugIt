bdee6f95f0a8f9d3163086d29871cfde7396a78b
ashutuptiwari <106370097+ashutuptiwari@users.noreply.github.com>
2023-04-15 18:20:23 +0530

    set host='0.0.0.0'

--- trial1.py
+++ trial1.py
@@ -50,4 +50,4 @@ def video_feed():
     return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False,host='0.0.0.0)
